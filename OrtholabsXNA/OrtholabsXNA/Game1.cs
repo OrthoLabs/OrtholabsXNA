fa888626ee1bbacacacc06aa15b937722986803e
Nia <OrthoneLabs@gmail.com>
2013-05-06 00:49:35 -0700

    Cleanup and Testing
    Set the screen size to a forced 512x512, load in the temporary
    'hero.png', and some temporary variables for moving the sprite around on
    the screen. Altered SpriteBatch from default to clamp to pixel.

--- OrtholabsXNA/OrtholabsXNA/Game1.cs
+++ OrtholabsXNA/OrtholabsXNA/Game1.cs
@@ -2,19 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace OrtholabsXNA
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -25,65 +18,59 @@ namespace OrtholabsXNA
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 512;
+            graphics.PreferredBackBufferHeight = 512;
+            graphics.ApplyChanges();
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            hero = this.Content.Load<Texture2D>("hero");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                heroPos.Y -= 1*(float)gameTime.ElapsedGameTime.TotalSeconds * speedMultiplier;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                heroPos.Y += 1 * (float)gameTime.ElapsedGameTime.TotalSeconds * speedMultiplier;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                heroPos.X -= 1 * (float)gameTime.ElapsedGameTime.TotalSeconds * speedMultiplier;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                heroPos.X += 1 * (float)gameTime.ElapsedGameTime.TotalSeconds * speedMultiplier;
+            }
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        private Texture2D hero;
+        private Vector2 heroPos;
+        private float speedMultiplier = 180f;
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, null);
+            spriteBatch.Draw(hero, heroPos, null, Color.White, 0f, Vector2.Zero, new Vector2(4, 4), SpriteEffects.None, 0f);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
