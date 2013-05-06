dbf149d7e4f519f64b39792c53076b032935897d
Nia <OrthoneLabs@gmail.com>
2013-05-06 00:47:58 -0700

    minor change for consistency

--- OrtholabsXNA/OrtholabsXNA/Program.cs
+++ OrtholabsXNA/OrtholabsXNA/Program.cs
@@ -10,7 +10,7 @@ namespace OrtholabsXNA
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (var game = new Game1())
             {
                 game.Run();
             }
