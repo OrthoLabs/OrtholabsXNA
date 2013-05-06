3a842067516de5775ad4a833a5bb987bab1bc40e
Nia <OrthoneLabs@gmail.com>
2013-05-06 01:32:51 -0700

    more temporary code
    - Modified the draw code to use the new spritebatch test instead of a
    single tile
    - Color[] textureBackgroundColors - Two default purple-tinted colors
    used as the main background texture for tilesheets. Used with the helper
    function 'RemoveColors' and the texture that you want to be transparent.
    - Added 3 helper functions
    * ColorMatch(Color a, Color b, bool checkTransparency = false) - Simply
    returns if the two colors match their RGB (and A, if you add true as a
    third statement) components
    * ReplaceColor(ref Texture2D texture, Color find, Color replace) - Goes
    through the entire texture and finds any of the 'find' color, and
    replaces it with the 'replace' color. The new data is then set back into
    the original texture.
    * RemoveColors(ref Texture2D texture, Color[] colors) - Goes through the
    entire texture, and replaces any instance that it finds in the list of
    colors with a new Color(0, 0, 0, 0). Primary use is to remove the
    background for spritesheets using the above non-temporary Color[]
    - Using the above helper functions, removes gridded background from
    spritesheet

--- OrtholabsXNA/OrtholabsXNA/Game1.cs
+++ OrtholabsXNA/OrtholabsXNA/Game1.cs
@@ -24,17 +24,23 @@ namespace OrtholabsXNA
             graphics.PreferredBackBufferHeight = 512;
             graphics.ApplyChanges();
 
+            //TEMPORARY CODE
             heroPos = new Vector2(graphics.PreferredBackBufferWidth/2f, graphics.PreferredBackBufferHeight/2f);
+            //END OF TEMPORARY CODE
 
             base.Initialize();
         }
 
+        public static Color[] textureBackgroundColors = new Color[] { new Color(176, 97, 255), new Color(176, 153, 255) };
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-            hero = this.Content.Load<Texture2D>("hero");
+            //TEMPORARY CODE
+            hero = this.Content.Load<Texture2D>("Textures/Mob/Player/heroTilesheet");
+
+            RemoveColors(ref hero, textureBackgroundColors);
+            //END OF TEMPORARY CODE
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,6 +48,7 @@ namespace OrtholabsXNA
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
+            //TEMPORARY CODE
             if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
                 heroPos.Y -= 1*(float)gameTime.ElapsedGameTime.TotalSeconds * speedMultiplier;
@@ -58,20 +65,73 @@ namespace OrtholabsXNA
             {
                 heroPos.X += 1 * (float)gameTime.ElapsedGameTime.TotalSeconds * speedMultiplier;
             }
+            //END OF TEMPORARY CODE
 
             base.Update(gameTime);
         }
 
+        //TEMPORARY CODE
         private Texture2D hero;
         private Vector2 heroPos;
         private const float speedMultiplier = 180f;
+
+        private void RemoveColors(ref Texture2D texture, Color[] colors)
+        {
+            var data = new Color[texture.Width * texture.Height];
+            texture.GetData(data);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                foreach (var color in colors)
+                {
+                    if (ColorMatch(data[i], color))
+                    {
+                        data[i] = new Color(0, 0, 0, 0);
+                    }
+                }
+            }
+
+            texture.SetData(data);
+        }
+        private void ReplaceColor(ref Texture2D texture, Color find, Color replace)
+        {
+            var data = new Color[texture.Width * texture.Height];
+            texture.GetData(data);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (ColorMatch(data[i], find))
+                {
+                    data[i] = replace;
+                }
+            }
+
+            texture.SetData(data);
+        }
+        private bool ColorMatch(Color a, Color b, bool checkTransparent = false)
+        {
+            if (a.R == b.R && a.G == b.G && a.B == b.B)
+            {
+                if(checkTransparent)
+                {
+                    return a.A == b.A;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        //END OF TEMPORARY CODE
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, null);
-            spriteBatch.Draw(hero, heroPos, null, Color.White, 0f, new Vector2(hero.Width/2f, hero.Height*.75f), new Vector2(4, 4), SpriteEffects.None, 0f);
+            //TEMPORARY CODE
+            spriteBatch.Draw(hero, heroPos, new Rectangle(0, 0, 24, 31), Color.White, 0f, new Vector2(24/2f, 31*.75f), new Vector2(4, 4), SpriteEffects.None, 0f);
+            //END OF TEMPORARY CODE
+
             spriteBatch.End();
 
             base.Draw(gameTime);
