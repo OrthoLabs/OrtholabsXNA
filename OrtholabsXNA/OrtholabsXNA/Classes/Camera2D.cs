d8b5c85661d9691eea512e1c8f2506db196acade
Nia <OrthoneLabs@gmail.com>
2013-05-07 21:54:58 -0700

    pre-made camera2D system

---
+++ OrtholabsXNA/OrtholabsXNA/Classes/Camera2D.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OrtholabsXNA
+{
+    public class Camera2D
+    {
+        public static Vector2 ScreenSize = new Vector2(512, 512);
+        protected float _zoom; // Camera Zoom
+        public Matrix _transform; // Matrix Transform
+        public Vector2 _pos; // Camera Position
+        protected float _rotation; // Camera Rotation
+
+        public Camera2D()
+        {
+            _zoom = 1.0f;
+            _rotation = 0.0f;
+            _pos = Vector2.Zero;
+        }
+
+        // Sets and gets zoom
+        public float Zoom
+        {
+            get { return _zoom; }
+            set { _zoom = value; if (_zoom < 0.1f) _zoom = 0.1f; } // Negative zoom will flip image
+        }
+
+        public float Rotation
+        {
+            get { return _rotation; }
+            set { _rotation = value; }
+        }
+
+        // Auxiliary function to move the camera
+        public void Move(Vector2 amount)
+        {
+            _pos += amount;
+        }
+        // Get set position
+        public Vector2 Pos
+        {
+            get { return _pos; }
+            set { _pos = value; }
+        }
+
+        public Matrix get_transformation(GraphicsDevice graphicsDevice)
+        {
+            _transform =       // Thanks to o KB o for this solution
+              Matrix.CreateTranslation(new Vector3(-_pos.X, -_pos.Y, 0)) *
+                                         Matrix.CreateRotationZ(Rotation) *
+                                         Matrix.CreateScale(new Vector3(Zoom, Zoom, 0)) *
+                                         Matrix.CreateTranslation(new Vector3(ScreenSize.X * 0.5f, ScreenSize.Y * 0.5f, 0));
+            return _transform;
+        }
+
+        public Vector2 get_mouse_vpos()
+        {
+            var mp = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+
+            return ((mp * this.Zoom) + this._pos); // Camera is a member of the class in this example
+        }
+    }
+}
